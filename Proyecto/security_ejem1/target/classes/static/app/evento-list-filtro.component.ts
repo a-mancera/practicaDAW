7bdd3ba51a309f7048dce39081acb60b682a0a0e
a-mancera <ricardo_1992_75@hotmail.com>
2016-06-30 16:19:51 +0200

    Se añade la posibilidad de poder consultar eventos utilizando filtros

--- Proyecto/security_ejem1/target/classes/static/app/evento-list-filtro.component.ts
+++ Proyecto/security_ejem1/target/classes/static/app/evento-list-filtro.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit}   from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteParams, Router} from 'angular2/router';
 
 import {Evento, EventoService}   from './evento.service';
-import {LoginService}   from './login.service';
+import {LoginService, TipoEvento}   from './login.service';
 
 export interface Filtro {  
     ciudad:string;
@@ -15,7 +15,14 @@ export interface Filtro {
     <h2>Filtro</h2>
     <label>Ciudad: </label>
     <input [(ngModel)]="filtro.ciudad" placeholder="Nombre de usuario"/><br>
+    <label>Tipos de evento:</label>	
+     <span *ngFor="#item of tiposEvento">
+		<input type="checkbox" [(ngModel)]="item.checked"/> {{item.tipo}}
+		</span>
+	<br>
     <button (click)="realizarFiltro()">Buscar</button>
+    
+    
     <div *ngIf="mostrar">
     <ul>
       <li *ngFor="#evento of eventosFiltrados">
@@ -37,9 +44,12 @@ export class EventoFiltroComponent implements OnInit {
 	filtro:Filtro;
 	mostrar = false;
 	resultados = false;
+	filtroPorTipos = false;
     eventos: Evento[];
 	eventosFiltrados = new Array<Evento>();
-    constructor(private service:EventoService, private router:Router) {
+	tiposEvento = new Array<TipoEvento>();
+	
+    constructor(private service:EventoService, private router:Router, private lService:LoginService) {
     	this.filtro = { ciudad:'',tipos:[] };
     }
 
@@ -48,18 +58,51 @@ export class EventoFiltroComponent implements OnInit {
         eventos => this.eventos = eventos,
         error => console.log(error)
       );
+      
+      this.lService.getTiposEventos().subscribe(
+	        tiposEvento => this.tiposEvento = tiposEvento,
+	        error => console.log(error)
+      );
     }
 	
 	realizarFiltro(){
+		this.resultados = false;
 		this.eventosFiltrados = new Array<Evento>();
-		for (let i of this.eventos){
+		for (let evento of this.eventos){
     		if (this.filtro.ciudad!=''){
-    			if (i.ciudad == this.filtro.ciudad){
-    				this.resultados = true;
-    				this.eventosFiltrados.push(i);
+    			if (evento.ciudad == this.filtro.ciudad){
+    				for (let i of this.tiposEvento){
+			    		if (i.checked){
+			    			this.filtroPorTipos = true;
+			    			if (i.tipo == evento.tipo){
+			    				this.resultados = true;
+    							this.eventosFiltrados.push(evento);		
+			    			}
+			    		}
+			    	}  
+			    	if (!this.filtroPorTipos){
+			    		this.resultados = true;
+    					this.eventosFiltrados.push(evento);		
+			    	}  				
     			}
-    		}
+    		}else{
+    				for (let i of this.tiposEvento){
+			    		if (i.checked){
+			    			if (i.tipo == evento.tipo){
+			    				this.resultados = true;
+    							this.eventosFiltrados.push(evento);		
+			    			}
+			    		}
+			    	}  
+    			}
     	}
 		this.mostrar = true;
+	}
+	
+	filtroPorTipo(){
+		for (let i of this.tiposEvento){
+    		if (i.checked){
+    			this.usuario.tiposEventos.push(i.tipo);
+    	}
 	}
 }
